39047758c0fdda556b01d517f7bcebc59bb35f17
wi1dcard <yokka47@qq.com>
2018-06-12 10:14:05 +0800

    Change a bit

--- tests/index.php
+++ tests/index.php
@@ -1,18 +1,28 @@
 <?php
 
+define('YII_DEBUG', true);
+
 require_once '../vendor/autoload.php';
+require_once '../vendor/yiisoft/yii2/Yii.php';
 
 use Hejiang\Express\Waybill;
 use Hejiang\Express\Trackers\Kuaidi100;
 use Hejiang\Express\Trackers\Kuaidiwang;
 use Hejiang\Express\Exceptions\TrackingException;
 
-$wb = new Waybill();
+$wb = \Yii::createObject(
+    [
+        'class' => 'Hejiang\Express\Waybill',
+        'id' => '70105425727637',
+        'express' => '百世汇通',
+    ]
+);
 
-$wb->id = '70105425727637';
-$wb->express = '百世汇通';
-
-$tracker = new Kuaidi100();
+$tracker = \Yii::createObject(
+    [
+        'class' => 'Hejiang\Express\Trackers\Kuaidi100',
+    ]
+);
 
 try {
     $traces = $wb->getTraces($tracker);
